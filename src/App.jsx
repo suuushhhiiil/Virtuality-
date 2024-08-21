b6614d001557adf116914d91e657f7af198eb322
suuushhhiiil <jangrasushiljnv1@gmail.com>
2024-08-21 11:45:49 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar.jsx';
 import HeroSection from './components/HeroSection.jsx';
 import FeatureSection from './components/FeatureSection.jsx';
+import Workflow from './components/Workflow.jsx';
+import Price from './components/Price.jsx';
+import Testimonials from './components/Testimonials.jsx';
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
    <div className='max-w-7xl mx-auto pt-10 px-6'>
    <HeroSection />
    <FeatureSection />
+   <Workflow />
+   <Price />
+   <Testimonials />
    </div>
    </>
   )
